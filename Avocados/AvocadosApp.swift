ee2cc9a4cabf45f230b649cbf01b2f8137e060bb
Light2288 <davide.aliti@gmail.com>
2022-08-03 00:16:52 +0200

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Davide Aliti on 03/08/22.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
